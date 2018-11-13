29f5a494ee826a4c900607f3633a2a5a6cbc5423
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-11-13 17:11:12 -0500

    add winner X and winner O tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -3,6 +3,8 @@
 // load previously computed paths
 const shortestValuePaths = require('../fixtures/shortest')
 const draws = require('../fixtures/draws')
+const winnerX = require('../fixtures/winner-x')
+const winnerO = require('../fixtures/winner-o')
 
 function deserializeEventString(eventString) {
   const [type, payload] = eventString.split(' | ');
@@ -20,9 +22,37 @@ const eventMap = {
   }
 };
 
+const play = (pathConfig) => {
+  for (const { state, event: eventString } of pathConfig) {
+    if (!eventString) {
+      continue;
+    }
+    const event = deserializeEventString(eventString);
+    const realEvent = eventMap[event.type];
+
+    if (realEvent) {
+      realEvent(event);
+    }
+  }
+}
+
 describe('Tic Tac Toe', () => {
   beforeEach(() => {
     cy.visit('/')
+    // this is how we know the app UI is ready
+    cy.contains('h2', /^Player X$/)
+  })
+
+  it('player X wins', () => {
+    const pathConfig = shortestValuePaths[winnerX[0]]
+    play(pathConfig)
+    cy.contains('h2', 'Player X wins!')
+  })
+
+  it('player O wins', () => {
+    const pathConfig = shortestValuePaths[winnerO[0]]
+    play(pathConfig)
+    cy.contains('h2', 'Player O wins!')
   })
 
   draws.forEach((targetStateString, k) => {
@@ -30,17 +60,7 @@ describe('Tic Tac Toe', () => {
       const pathConfig = shortestValuePaths[targetStateString]
       console.log('testing', pathConfig)
 
-      for (const { state, event: eventString } of pathConfig) {
-        if (!eventString) {
-          continue;
-        }
-        const event = deserializeEventString(eventString);
-        const realEvent = eventMap[event.type];
-
-        if (realEvent) {
-          realEvent(event);
-        }
-      }
+      play(pathConfig)
 
       // now we should get a draw message
       cy.contains('h2', 'Draw')
